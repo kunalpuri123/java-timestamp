d465de913d4a8fd3ac20b3d2decd6160e07ce8c9
kunalpuri123 <kunalpuri164@gmail.com>
2025-02-11 13:43:43 +0530

    Initial commit with Java program

---
+++ Timestamp.java
@@ -0,0 +1,10 @@
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Timestamp {
+    public static void main(String[] args) {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String timestamp = sdf.format(new Date());
+        System.out.println("Current Timestamp: " + timestamp);
+    }
+}
